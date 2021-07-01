84cd5decb1579b7f085ee0288b1e97b6e335de67
BjornJaupllari <72342845+BjornJaupllari@users.noreply.github.com>
2021-07-01 13:30:27 +0200

    BJ/ Structure

--- ATS/src/main/java/com/example/ATS/repository/RoleRepository.java
+++ ATS/src/main/java/com/example/ATS/repository/RoleRepository.java
@@ -1,6 +1,9 @@
 package com.example.ATS.repository;
 
+import com.example.ATS.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface RoleRepository extends JpaRepository {
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
 }
