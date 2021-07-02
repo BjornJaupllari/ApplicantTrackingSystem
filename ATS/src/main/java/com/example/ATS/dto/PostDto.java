5f5a2f9da362b78e46f4b433310a7c8bff833792
BjornJaupllari <72342845+BjornJaupllari@users.noreply.github.com>
2021-07-02 15:22:28 +0200

    BJ

--- ATS/src/main/java/com/example/ATS/dto/PostDto.java
+++ ATS/src/main/java/com/example/ATS/dto/PostDto.java
@@ -1,4 +1,26 @@
 package com.example.ATS.dto;
 
+import com.example.ATS.entity.Company;
+import com.example.ATS.entity.Interview;
+import com.example.ATS.entity.Type;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class PostDto {
+
+    private int id;
+    private String contact;
+    private String jobTitle;
+    private Date expirationDate;
+    private Type type;
+    private int workExperience;
+    private String department;
+    private Byte[] attachment;
+    private String description;
+    private String location;
+    private Company company;
+    private Interview interview;
+
 }
