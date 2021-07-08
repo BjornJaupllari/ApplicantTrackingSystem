8a3874be9d67fa96da197cd9d28b776f9c3a900a
BjornJaupllari <72342845+BjornJaupllari@users.noreply.github.com>
2021-07-08 14:38:50 +0200

    user

--- ATS/src/main/java/com/example/ATS/AtsApplication.java
+++ ATS/src/main/java/com/example/ATS/AtsApplication.java
@@ -2,12 +2,15 @@ package com.example.ATS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+
 public class AtsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AtsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AtsApplication.class, args);
+    }
 
 }
--- ATS/src/main/java/com/example/ATS/entity/Type.java
+++ ATS/src/main/java/com/example/ATS/entity/Type.java
@@ -1,25 +1,25 @@
-package com.example.ATS.entity;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-import javax.persistence.*;
-import java.io.Serializable;
-
-@Entity
-@Table(name = "type")
-@Getter
-@NoArgsConstructor
-@ToString
-public class Type implements Serializable {
-
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    @Column(name = "name")
-    private String name;
-
-}
+//package com.example.ATS.entity;
+//
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.ToString;
+//
+//import javax.persistence.*;
+//import java.io.Serializable;
+//
+//@Entity
+//@Table(name = "type")
+//@Getter
+//@NoArgsConstructor
+//@ToString
+//public class Type implements Serializable {
+//
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int id;
+//
+//    @Column(name = "name")
+//    private String name;
+//
+//}
